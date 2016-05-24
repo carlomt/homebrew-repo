2d2e95a78b6e75bcb5ce33a64660e45282cc001d
Carlo Mancini <carlo@accatagliato.org>
2016-05-25 00:37:51 +0200

    test pydicom

--- pydicom.rb
+++ pydicom.rb
@@ -1,8 +1,10 @@
 class Pydicom < Formula
   desc "a python package for working with DICOM files"
   homepage "https://github.com/darcymason/pydicom"
-  url "https://github.com/darcymason/pydicom/archive/master.zip"
-  
+  url "https://github.com/darcymason/pydicom/archive/v0.9.9.zip"
+  version "0.9.9"
+  head "https://github.com/darcymason/pydicom.git"
+ 
   depends_on "python"
   depends_on "numpy" => :python
   
