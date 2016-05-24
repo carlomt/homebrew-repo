87fab3595742069c35783e3e5b02ee5b69162089
Carlo Mancini <carlo@accatagliato.org>
2016-05-25 00:51:56 +0200

    test pydicom

--- flair.rb
+++ flair.rb
@@ -6,7 +6,7 @@ class Flair < Formula
   version "2.2-1"
   sha256  "963d7cb1935bd626767459f5c0765e590653a2d23eea81ff5f9b9aabcc1a1916"
 
-  option "pydicom" => :recommended, "add the pydicom package to brewed python" 
+  option "pydicom", "add the pydicom package to brewed python" 
   
   # depends_on "cmake" => :build
   depends_on :x11 
