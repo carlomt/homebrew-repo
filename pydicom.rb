b213ab735c62d7725d94fd2a24b81f09fd06f10e
Carlo Mancini <carlo@accatagliato.org>
2016-05-25 00:34:46 +0200

    test pydicom

--- pydicom.rb
+++ pydicom.rb
@@ -2,12 +2,11 @@ class Pydicom < Formula
   desc "a python package for working with DICOM files"
   homepage "https://github.com/darcymason/pydicom"
   url "https://github.com/darcymason/pydicom/archive/master.zip"
-  end
-
+  
   depends_on "python"
   depends_on "numpy" => :python
-
-
+  
+  
   def install
     ENV.prepend_create_path "PYTHONPATH", libexec/"vendor/lib/python2.7/site-packages"
     system "python", *Language::Python.setup_install_args(prefix)
